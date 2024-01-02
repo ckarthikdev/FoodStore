15e5338b866acabeebdb33942ec41853c1359d46
ckarthikdev <116702032+ckarthikdev@users.noreply.github.com>
2024-01-02 16:50:32 +0530

    updated code

--- 03-steps/src/challenge/StepCounter.js
+++ 03-steps/src/challenge/StepCounter.js
@@ -1,1 +1,43 @@
+import { useState } from "react";
 
+function StepCounter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  //  *** For Date
+  const date = new Date();
+  // To Update the Date with new date
+  // const currDate = date.getDate(); To get the current date
+  date.setDate(date.getDate() + count); // Update the date
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1>Counter</h1>
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is  "
+            : count > 0
+            ? `${count} days from from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
+
+export default StepCounter;
